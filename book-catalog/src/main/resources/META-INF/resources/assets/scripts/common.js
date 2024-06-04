187502936889130207a8103e59f627a8aee0989b
RoboBaranik <robobaranik3000@gmail.com>
2024-06-04 16:30:06 +0200

    Add CORS

--- book-catalog/src/main/resources/META-INF/resources/assets/scripts/common.js
+++ book-catalog/src/main/resources/META-INF/resources/assets/scripts/common.js
@@ -1,5 +1,6 @@
 function onLoad() {
   loadStreams();
+  pingLibraryService();
 }
 
 
@@ -13,4 +14,17 @@ function loadStreams() {
     const book = JSON.parse(event.data);
     console.log('Received new Book from Event stream', book);
   };
+}
+
+async function pingLibraryService() {
+  const response = await fetch("http://localhost:8081/api", {
+    method: "GET"
+  });
+  if (response.status != 200) {
+    console.error("Unable to connect");
+    return;
+  }
+
+  const responseBody = await response.text();
+  console.log("Pinged LibraryService", responseBody);
 }
